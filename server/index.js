18643365019f0399fb10cc0655b6149d11b45ced
talikou <ulykbek.talant2@gmail.com>
2023-05-26 16:38:03 +0300

    :white_check_mark: Server part completed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const router = require('./router')
+const errorMiddleware = require('./middleware/error-middleware')
 
 const PORT = process.env.PORT || 8000
 const app = express()
@@ -12,6 +13,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 app.use('/api', router)
+app.use(errorMiddleware)
+
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const userController = require('../controller/user-controller')
 const router = express.Router()
+const { body } = require('express-validator')
+const authMiddleware = require('../middleware/auth-middleware')
 
-router.post('/registration', userController.registration)
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 3, max: 32 }),
+  userController.registration
+)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.post('/refresh', userController.refresh)
-router.get('/users', userController.getUsers)
+router.get('/users', authMiddleware, userController.getUsers)
 
 module.exports = router
